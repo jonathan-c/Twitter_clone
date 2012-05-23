7ffe656daa561569ce0fcc846e12c2bcdb121226
Jonathan Castillo <jonathan@ygm.com>
2012-05-22 23:52:04 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.2'
+
+group :development do
+	gem 'sqlite3', '1.3.5'
+  gem 'rspec-rails', '2.10.0'
+end
+
+# Gems used only for assets and not required
+# in production environment by default.
+
+group :assets do
+	gem 'sass-rails', '3.2.4'
+	gem 'coffee-rails', '3.2.2'
+	gem 'uglifier', '1.2.3'
+end
+
+gem 'jquery-rails', '2.0.0'
+
+group :test do
+	gem 'rspec-rails', '2.10.0'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HartlTutorial::Application.config.secret_token = '0dfa87a6594ba1c274a0a1fac77ba3093464b3e95ee4a4c655b954cf54b0c23c82149983780a2f8be42b7dd4fe520d430bcd3768e776874ca050e8b4c0cfd52d'
