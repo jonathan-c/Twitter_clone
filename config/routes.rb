0bedabbf5632e6036f976f2c25c0457c642c017f
Jonathan Castillo <jonathan@ygm.com>
2012-06-05 14:15:20 -0400

    Added sessions controller and signin view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 HartlTutorial::Application.routes.draw do
-  
+
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   root to: 'pages#home'
   
@@ -9,6 +10,8 @@ HartlTutorial::Application.routes.draw do
   match '/help',        to: 'pages#help'
   match '/advertising', to: 'pages#advertising'
   match '/signup',      to: 'users#new'
+  match '/signin',      to: 'sessions#new'
+  match '/signout',     to: 'sessions#destroy'
   
 
   # The priority is based upon order of creation:
