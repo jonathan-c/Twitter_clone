9eb0ea0e7aa7a38536f301b0abab017522d8b170
Jonathan Castillo <jonathan@ygm.com>
2012-06-05 15:17:56 -0400

    Added sessions create

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@
 #  created_at         :datetime        not null
 #  updated_at         :datetime        not null
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 require 'spec_helper'
