a8de2dca79c9ad4b4a1faa98f3f030947478fd18
Jonathan Castillo <jonathan@ygm.com>
2012-06-07 14:14:25 -0400

    Added microposts model with user associations

---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+require 'spec_helper'
+
+describe Micropost do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
