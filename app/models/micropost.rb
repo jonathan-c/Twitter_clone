fc1f6c947f972a5d98ed0d5d80226d97a1594b7b
Jonathan Castillo <jonathan@ygm.com>
2012-06-07 14:18:19 -0400

    Micropost validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -13,5 +13,11 @@ class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
   
+  validates :content, presence: true,
+                      length: { maximum: 140 }
+                      
+  validates :user_id, presence: true
+  
   default_scope order: 'microposts.created_at DESC'
+  
 end
