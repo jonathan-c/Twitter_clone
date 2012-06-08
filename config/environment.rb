f9af03dc5434a88c920b979b04e38c55c66ffcec
Jonathan Castillo <jonathan@ygm.com>
2012-06-07 20:06:29 -0400

    silenced deprecation in production.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-ActiveSupport::Deprecation.silenced = true 
 
 # Initialize the rails application
 HartlTutorial::Application.initialize!
