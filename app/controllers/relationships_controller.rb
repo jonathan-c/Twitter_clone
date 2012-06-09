e697bc8f8cd4b4f467cd320b78e89eafa5a60471
Jonathan Castillo <jonathan@ygm.com>
2012-06-08 22:04:07 -0400

    Made a working feed

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,25 +6,23 @@ class RelationshipsController < ApplicationController
     current_user.follow!(user)
     redirect_to(user)
   end
-  
+  # 
   # def create
   #   @user = User.find(params[:relationship][:followed_id])
   #   current_user.follow!(@user)
-  #   #respond_to do |format|
-  #   #  format.html { redirect_to(@user) }
-  #   #  format.js
-  #   #end
-  #   redirect_to(@user)
+  #   respond_to do |format|
+  #        format.html { redirect_to(@user) }
+  #        format.js
+  #   end
   # end
-  # 
+  
   # def destroy
   #   @user = Relationship.find(params[:id]).followed
   #   current_user.unfollow!(@user)
-  #   #respond_to do |format|
-  #   #  format.html { redirect_to(@user) }
-  #   #  format.js
-  #   #end
-  #   redirect_to(@user)
+  #   respond_to do |format|
+  #    format.html { redirect_to(@user) }
+  #    format.js
+  #   end
   # end
   
   def destroy
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -10,7 +10,7 @@
 #
 
 class Micropost < ActiveRecord::Base
-  attr_accessible :content, :user_id
+  attr_accessible :content
   belongs_to :user
   
   validates :content, presence: true,
@@ -20,4 +20,13 @@ class Micropost < ActiveRecord::Base
   
   default_scope order: 'microposts.created_at DESC'
   
+  scope :from_users_followed_by, lambda { |user| followed_by(user) }
+  
+  private
+  
+  def self.followed_by(user)
+    followed_ids = %(SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id)
+    where("user_id IN (#{followed_ids}) OR user_id = :user_id", user_id: user)
+  end
 end
